ffc56cb37669b106a86bc96d47a77e4d54b97bd8
Pauline Wang <paulinewang@Paulines-MacBook-Pro.local>
2026-02-15 16:02:32 +0100

    update residentsselector logic; update styles

--- web/react-example/src/components/ResidentsSelector.tsx
+++ web/react-example/src/components/ResidentsSelector.tsx
@@ -1,26 +1,34 @@
-import styles from './ResidentsSelector.module.css';
-import { People1, People2, People3, People4, People5 } from './icons';
+import styles from "./ResidentsSelector.module.css";
+import { People1, People2, People3, People4, People5 } from "./icons";
 
-const residentOptions = [<People1 />, <People2 />, <People3 />, <People4 />, <People5 />]
+const residentOptions = [
+  <People1 />,
+  <People2 />,
+  <People3 />,
+  <People4 />,
+  <People5 />,
+];
 
 interface ResidentsSelectorProps {
   value: number;
   onChange: (value: number) => void;
 }
 
+export default function ResidentsSelector({
+  value,
+  onChange,
+}: ResidentsSelectorProps) {
+  console.log({ value });
 
-export default function ResidentsSelector({ value, onChange }: ResidentsSelectorProps) {
   return (
     <div className={styles.container}>
       {residentOptions.map((residentOption, index) => (
         <button
           key={`residentOption-${index + 1}`}
           onClick={() => onChange(index + 1)}
-          className={`${styles.button} ${value === index ? styles.selected : ''}`}
+          className={`${styles.button} ${value === index + 1 ? styles.selected : ""}`}
         >
-          <div className={styles.icons}>
-            {residentOption}
-          </div>
+          <div className={styles.icons}>{residentOption}</div>
         </button>
       ))}
     </div>
